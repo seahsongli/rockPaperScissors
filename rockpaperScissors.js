099c1eea0a959fb32fff119f8ff5267c67d1d8ea
Philedo <seahsongli@gmail.com>
2022-07-09 18:08:32 +0800

    Add conditions where result == null or false. Add results for invalid tokens/inputs.

--- rockpaperScissors.js
+++ rockpaperScissors.js
@@ -10,7 +10,7 @@ function getRandomNumber(min,max) {
 
 // Function to get a random play from computer
 function computerPlay(){  
-      let computerChoices = ["rock", "paper", "scissors"];
+      const computerChoices = ["rock", "paper", "scissors"];
       let randomIndex = getRandomNumber(0,2);
       let result = computerChoices[randomIndex]
       return result;
@@ -28,19 +28,52 @@ function capitalizeFirstLetter(str){
 
 // Function to get the round to start playing
 function playRound(playerSelection,computerSelection) {
-    playerSelection.toLowerCase()
-    if (playerSelection == computerSelection){
-        return(`It is a tie!${capitalizeFirstLetter(computerSelection)}(computer) vs ${capitalizeFirstLetter(playerSelection)}(player).`)
-    }
 
-    else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock"))  {
-        return(`You lose! ${capitalizeFirstLetter(computerSelection)}(computer) beats ${capitalizeFirstLetter(playerSelection)}(player).`)
+   //If player cancel or did not type antyhing
+    if(playerSelection == null || playerSelection == false ) { 
+        return ("Invalid choice! Please try again!")
+        
     }
     else {
-        return(`You Win! ${capitalizeFirstLetter(playerSelection)}(player) beats ${capitalizeFirstLetter(computerSelection)}(computer).`)
+        playerSelection.toLowerCase()
+
+        if (playerSelection == computerSelection) 
+        {
+            return(`It is a tie! ${capitalizeFirstLetter(computerSelection)}(computer) vs ${capitalizeFirstLetter(playerSelection)}(player).`);
+        }
+        
+        else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock"))  
+        {
+            return(`You lose! ${capitalizeFirstLetter(computerSelection)}(computer) beats ${capitalizeFirstLetter(playerSelection)}(player).`);
+        }
+
+        else if ((computerSelection == "rock" && playerSelection == "paper") || (computerSelection == "paper" && playerSelection == "scissors") || (computerSelection == "scissors" && playerSelection == "rock"))
+        {
+            return(`You Win! ${capitalizeFirstLetter(playerSelection)}(player) beats ${capitalizeFirstLetter(computerSelection)}(computer).`);
+        }
+
+        else 
+        {
+            return ("Invalid choice! Please try again!");
+        }
     }
+
+}
+let outcome;
+function game(){
+    for (i=0; i< 5; i++){
+        const playerSelection = prompt("Choose Rock, Paper or Scissors!");
+        const computerSelection = computerPlay();
+        outcome = playRound(playerSelection,computerSelection);
+        if (outcome == "Invalid choice! Please try again!" ){
+            i--; //so that it can be out of 5 times
+        }
+        alert(outcome);
+        console.log(outcome);
+    }
+    return;
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+
+game();
