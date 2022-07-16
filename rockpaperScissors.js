c3996e080197041938106f8867ac07cb72da61f2
Philedo <seahsongli@gmail.com>
2022-07-16 15:21:05 +0800

    Add buttons for players to use. Add event listeners to the buttons. Add counter for player/computer scores. Add function to declare winner.

--- rockpaperScissors.js
+++ rockpaperScissors.js
@@ -28,6 +28,7 @@ function capitalizeFirstLetter(str){
 
 // Function to get the round to start playing
 function playRound(playerSelection,computerSelection) {
+    
 
    //If player cancel or did not type antyhing
     if(playerSelection == null || playerSelection == false ) { 
@@ -35,7 +36,7 @@ function playRound(playerSelection,computerSelection) {
         
     }
     else {
-        playerSelection.toLowerCase()
+       
 
         if (playerSelection == computerSelection) 
         {
@@ -44,11 +45,13 @@ function playRound(playerSelection,computerSelection) {
         
         else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock"))  
         {
+            computerScore += 1;
             return(`You lose! ${capitalizeFirstLetter(computerSelection)}(computer) beats ${capitalizeFirstLetter(playerSelection)}(player).`);
         }
 
         else if ((computerSelection == "rock" && playerSelection == "paper") || (computerSelection == "paper" && playerSelection == "scissors") || (computerSelection == "scissors" && playerSelection == "rock"))
         {
+            playerScore += 1;
             return(`You Win! ${capitalizeFirstLetter(playerSelection)}(player) beats ${capitalizeFirstLetter(computerSelection)}(computer).`);
         }
 
@@ -56,24 +59,66 @@ function playRound(playerSelection,computerSelection) {
         {
             return ("Invalid choice! Please try again!");
         }
+        
     }
+    declareWinner(computerScore, playerScore);
 
 }
-let outcome;
-function game(){
-    for (i=0; i< 5; i++){
-        const playerSelection = prompt("Choose Rock, Paper or Scissors!");
-        const computerSelection = computerPlay();
-        outcome = playRound(playerSelection,computerSelection);
-        if (outcome == "Invalid choice! Please try again!" ){
-            i--; //so that it can be out of 5 times
+
+const body = document.querySelector('body');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+
+let computerScore = 0;
+let playerScore = 0;
+const div = document.createElement('div');
+//everytime player clicks on button, computerPlay() will be called and playRound will be called for results to appear
+rock.addEventListener('click',function(){
+    const computerSelection = computerPlay();
+    const outcome = playRound("rock",computerSelection);
+    rock.classList.add ="btn1";
+    div.innerHTML= `${outcome}` + "<br />" + `Computer:${computerScore}, Player:${playerScore}`;
+    declareWinner(computerScore,playerScore);
+
+
+});
+body.append(div);
+
+paper.addEventListener('click',function(){
+    const computerSelection = computerPlay();
+    outcome = playRound("paper",computerSelection);
+    div.innerHTML= `${outcome}` + "<br />" + `Computer:${computerScore}, Player:${playerScore}`;
+    declareWinner(computerScore,playerScore);
+
+   
+});
+
+scissors.addEventListener('click',function(){
+    const computerSelection = computerPlay();
+    outcome = playRound("scissors",computerSelection);
+    div.innerHTML= `${outcome}` + "<br />" + `Computer:${computerScore}, Player:${playerScore}`;
+    declareWinner(computerScore,playerScore);
+    
+});
+
+
+// to determine winner
+function declareWinner(computerScore, playerScore){
+    if (computerScore == 5 || playerScore == 5){
+        if (computerScore > playerScore) {
+            alert("You lost! Better luck next time!");
+        
         }
-        alert(outcome);
-        console.log(outcome);
+        else {
+            alert("Congratulations! You won!");
+           
+        }
     }
-    return;
+   
+    
 }
 
 
 
-game();
+// game();
